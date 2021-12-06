d12b9aba05bd2017b1baeb8fcbeb9f6efae8922c
David Vargas <dvargas92495@gmail.com>
2021-12-05 20:34:28 -0500

    convertkit signup

--- pages/_common/Layout.tsx
+++ pages/_common/Layout.tsx
@@ -3,7 +3,18 @@ import DefaultLayout from "@dvargas92495/ui/dist/components/Layout";
 import { Head as DefaultHead } from "@dvargas92495/ui/dist/components/Document";
 
 const Layout: React.FC = ({ children }) => {
-  return <DefaultLayout homeIcon={"Home"}>{children}</DefaultLayout>;
+  return (
+    <DefaultLayout
+      homeIcon={"Home"}
+      themeProps={{
+        primary: "#316BFF",
+        secondary: "#ff00ff",
+        background: "#F7F8FC",
+      }}
+    >
+      {children}
+    </DefaultLayout>
+  );
 };
 
 type HeadProps = Omit<Parameters<typeof DefaultHead>[0], "title">;
@@ -12,9 +23,7 @@ export const LayoutHead = ({
   title = "Welcome",
   ...rest
 }: HeadProps & { title?: string }): React.ReactElement => {
-  return (
-    <DefaultHead title={`${title} | crowdinvestin`} {...rest} />
-  );
+  return <DefaultHead title={`${title} | crowdinvestin`} {...rest} />;
 };
 
 export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Layout, { LayoutHead } from "./_common/Layout";
+import ConvertKit from "@dvargas92495/ui/dist/components/ConvertKit";
 
-const Home: React.FC = () => <Layout>Welcome!</Layout>;
+const Home: React.FC = () => (
+  <Layout>
+    <ConvertKit id={"eb07e20bc3"} />
+  </Layout>
+);
 
 export const Head = () => <LayoutHead title={"Home"} />;
 
