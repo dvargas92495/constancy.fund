c6bf45359083912f21d24423cf31bb237bf32ea3
David Vargas <dvargas92495@gmail.com>
2021-12-06 21:39:39 -0500

    User Profile

--- pages/_common/Layout.tsx
+++ pages/_common/Layout.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import DefaultLayout from "@dvargas92495/ui/dist/components/Layout";
 import { Head as DefaultHead } from "@dvargas92495/ui/dist/components/Document";
 
+export const themeProps = {
+  primary: "#316BFF",
+  secondary: "#ff00ff",
+  background: "#F7F8FC",
+};
+
 const Layout: React.FC = ({ children }) => {
   return (
-    <DefaultLayout
-      homeIcon={"Home"}
-      themeProps={{
-        primary: "#316BFF",
-        secondary: "#ff00ff",
-        background: "#F7F8FC",
-      }}
-    >
+    <DefaultLayout homeIcon={"Home"} themeProps={themeProps}>
       {children}
     </DefaultLayout>
   );
@@ -23,7 +22,7 @@ export const LayoutHead = ({
   title = "Welcome",
   ...rest
 }: HeadProps & { title?: string }): React.ReactElement => {
-  return <DefaultHead title={`${title} | crowdinvestin`} {...rest} />;
+  return <DefaultHead title={`${title} | CrowdInvestInMe`} {...rest} />;
 };
 
 export default Layout;
--- pages/user.tsx
+++ pages/user.tsx
@@ -1,22 +1,157 @@
-import React from "react";
-import Layout, { LayoutHead } from "./_common/Layout";
+import { LayoutHead, themeProps } from "./_common/Layout";
+import Document from "@dvargas92495/ui/dist/components/Document";
 import RedirectToLogin from "@dvargas92495/ui/dist/components/RedirectToLogin";
 import clerkUserProfileCss from "@dvargas92495/ui/dist/clerkUserProfileCss";
-import { SignedIn, UserProfile } from "@clerk/clerk-react";
+import React, { useState } from "react";
+import { SignedIn,  UserButton, UserProfile } from "@clerk/clerk-react";
+import Drawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Link from "@mui/material/Link";
+import HomeIcon from "@mui/icons-material/Home";
+import ContractIcon from "@mui/icons-material/Note";
+import SettingsIcon from "@mui/icons-material/Settings";
+import H1 from "@dvargas92495/ui/dist/components/H1";
+import GlobalStyles from "@mui/material/GlobalStyles";
 
-const UserPage: React.FunctionComponent = () => (
-  <Layout>
-    <SignedIn>
+const ProfileContent = () => {
+  return (
+    <>
+      <H1>Profile</H1>
+      <style>{`.cl-component.cl-user-profile nav.cl-navbar, .cl-component div.cl-page-heading {
+  display: none;
+}
+
+.cl-component.cl-user-profile div.cl-content {
+  margin: unset;
+  padding-bottom: 2em;
+  font-family: "Century Gothic", sans-serif
+}`}</style>
       <UserProfile />
+    </>
+  );
+};
+
+const DRAWER_WIDTH = 255;
+const TABS = [
+  { text: "My Profile", Icon: HomeIcon, content: ProfileContent },
+  {
+    text: "My Contracts",
+    Icon: ContractIcon,
+    content: () => <div>Coming Soon!</div>,
+  },
+  {
+    text: "Settings",
+    Icon: SettingsIcon,
+    content: () => <div>Coming Soon!</div>,
+  },
+] as const;
+
+const Dashboard = () => {
+  const [tab, setTab] = useState(0);
+  const TabContent = TABS[tab].content;
+  return (
+    <Box sx={{ display: "flex", height: "100%" }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: `calc(100% - ${DRAWER_WIDTH}px)`,
+          ml: `${DRAWER_WIDTH}px`,
+          backgroundColor: themeProps.background,
+        }}
+        elevation={0}
+      >
+        <Toolbar sx={{ display: "flex", justifyContent: "end" }}>
+          <UserButton />
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: DRAWER_WIDTH,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: DRAWER_WIDTH,
+            boxSizing: "border-box",
+            backgroundColor: "#363740",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar>
+          <Link href="/">Home</Link>
+        </Toolbar>
+        <List>
+          {TABS.map(({ text, Icon }, index) => (
+            <ListItem
+              button
+              key={index}
+              onClick={() => {
+                setTab(index);
+              }}
+              sx={{
+                display: "flex",
+                background: index === tab ? "#0000000a" : "unset",
+                borderLeft: index === tab ? "2px solid #DDE2FF" : "unset",
+                paddingLeft: "32px",
+                py: "20px",
+                fontSize: 14,
+                color: "#A4A6B3",
+              }}
+            >
+              <ListItemIcon sx={{ color: "inherit" }}>
+                <Icon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          color: "text.primary",
+        }}
+        flexDirection={"column"}
+        display={"flex"}
+      >
+        <Toolbar />
+        <Box flexGrow={1} display={"flex"} flexDirection={"column"}>
+          <TabContent />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+const globalStyles = (
+  <GlobalStyles
+    styles={{
+      html: { height: "100%" },
+      body: { height: "100%" },
+      "body > div[data-reactroot]": { height: "100%" },
+    }}
+  />
+);
+
+const UserPage = (): React.ReactElement => (
+  <Document themeProps={themeProps}>
+    {globalStyles}
+    <SignedIn>
+      <Dashboard />
     </SignedIn>
     <RedirectToLogin />
-  </Layout>
+  </Document>
 );
 
 export const Head = (): React.ReactElement => (
-  <LayoutHead
-    title={"User"}
-    styles={clerkUserProfileCss}
-  />
+  <LayoutHead title={"User"} styles={clerkUserProfileCss} />
 );
 export default UserPage;
