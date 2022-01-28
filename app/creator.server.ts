52277dcae4b214252108b47e643d4f6d96dc30b0
David Vargas <dvargas92495@gmail.com>
2022-01-28 11:56:55 -0600

    offload prisma

--- app/creator.server.ts
+++ app/creator.server.ts
@@ -1,58 +1,18 @@
-import { users } from "@clerk/clerk-sdk-node";
-import formatError from "@dvargas92495/api/formatError";
-import prisma from "../functions/_common/prisma";
+import type { Handler as GetHandler } from "../functions/creator-profile/get";
+import axios from "axios";
 
-export type Props = {
-  fullName: string;
-  email: string;
-  profileImageUrl: string;
-  socialProfiles: string[];
-  questionaires: string[];
-  fundraises: { type: number; uuid: string }[];
-};
+export type Props = Awaited<ReturnType<GetHandler>>;
 
 const getStaticProps = ({
   params: { id },
 }: {
   params: { id: string };
-}): Promise<{
-  props: Props;
-}> => {
-  return Promise.all([
-    users.getUser(id),
-    prisma.contract.findMany({ where: { userId: id } }),
-  ])
-    .then(([u, fs]) => ({
-      props: {
-        fullName: `${u.firstName} ${
-          typeof u.publicMetadata.middleName === "string"
-            ? `${u.publicMetadata.middleName.slice(0, 1)}. `
-            : ""
-        }${u.lastName}`,
-        email:
-          u.emailAddresses.find((e) => e.id === u.primaryEmailAddressId)
-            ?.emailAddress || "",
-        profileImageUrl: u.profileImageUrl || "",
-        socialProfiles: (
-          (u.publicMetadata.socialProfiles as string[]) || []
-        ).filter((s) => !!s),
-        questionaires: u.publicMetadata.questionaires as string[],
-        fundraises: fs.map((f) => ({ type: f.type, uuid: f.uuid })),
-      },
-    }))
-    .catch((e) => {
-      console.error(formatError(e));
-      return {
-        props: {
-          fullName: ``,
-          email: "",
-          profileImageUrl: "",
-          socialProfiles: [],
-          questionaires: [],
-          fundraises: [],
-        },
-      };
-    });
+}): Promise<{ props: Props }> => {
+  return axios
+    .get<Props>(`${process.env.API_URL}/creator-profile?id=${id}`)
+    .then((r) => ({
+      props: r.data,
+    }));
 };
 
 export default getStaticProps;
