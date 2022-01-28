d069eb063a589a904117cb2ed75eb600a2fbebea
David Vargas <dvargas92495@gmail.com>
2022-01-28 13:04:50 -0600

    fix asset loading and on cf error

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import { createRequestHandler } from "@dvargas92495/remix-lambda-at-edge";
+import sendEmail from "aws-sdk-plus/dist/sendEmail";
 
 export const handler = createRequestHandler({
   build: require("./build"),
@@ -8,4 +9,10 @@ export const handler = createRequestHandler({
     /^\/payment-options\/.*/,
     /^\/_contracts\/.*/,
   ],
+  onError: (e) =>
+    sendEmail({
+      to: "support@crowdinvestin.me",
+      subject: "Remix Origin Error",
+      body: e.message,
+    }),
 });
