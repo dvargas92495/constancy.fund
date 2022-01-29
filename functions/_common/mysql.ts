6fc3b99d02d00daaac45cc796603db3e8c7add0a
David Vargas <dvargas92495@gmail.com>
2022-01-29 17:11:20 -0600

    fix database regex

--- functions/_common/mysql.ts
+++ functions/_common/mysql.ts
@@ -1,7 +1,7 @@
 import mysql from "mysql2";
 
 const DATABASE_URL_REGEX =
-  /^mysql:\/\/([a-z0-9]+):(.{16})@([a-z0-9\.-]+):(\d{3,5})\/([a-z_]+)$/;
+  /^mysql:\/\/([a-z0-9_]+):(.{16})@([a-z0-9\.-]+):(\d{3,5})\/([a-z_]+)$/;
 const matches = DATABASE_URL_REGEX.exec(process.env.DATABASE_URL || "");
 
 if (!matches) throw new Error(`Error parsing Database URL`);
