e61a0a4e4f8ab92724a69a53a182242ef84fb92f
David Vargas <dvargas92495@gmail.com>
2022-01-29 20:50:02 -0600

    try the prismaless version of get fundraises

--- functions/fundraises/get.ts
+++ functions/fundraises/get.ts
@@ -1,46 +1,64 @@
 import clerkAuthenticateLambda from "@dvargas92495/api/clerkAuthenticateLambda";
 import createAPIGatewayProxyHandler from "aws-sdk-plus/dist/createAPIGatewayProxyHandler";
-import prisma from "../_common/prisma";
+import { execute } from "../_common/mysql";
 import type { User } from "@clerk/clerk-sdk-node";
 import FUNDRAISE_TYPES from "../../db/fundraise_types";
 
 const logic = ({ user: { id } }: { user: User }) => {
-  return prisma.contract
-    .findMany({
-      where: { userId: id || "" },
-      select: {
-        type: true,
-        uuid: true,
-        agreements: {
-          select: {
-            uuid: true,
-            eversign: {
-              select: {
-                id: true
-              }
-            }
-          },
-        },
-        contractDetails: {
-          select: {
-            label: true,
-            value: true,
-            uuid: true,
-          },
-        },
-      },
-    })
-    .then((fundraises) => {
-      return {
-        fundraises: fundraises.map((f) => ({
-          uuid: f.uuid,
-          type: FUNDRAISE_TYPES[f.type].id,
-          investorCount: f.agreements.length,
-          details: f.contractDetails,
-          progress: 0,
-        })),
-      };
+  return execute(
+    `SELECT c.type, c.uuid, a.uuid as agreementUuid, cd.label, cd.value, cd.uuid as cdUuid
+     FROM contract c
+     INNER JOIN agreement a ON a.contractUuid = c.uuid
+     INNER JOIN contractdetail cd ON a.contractUuid = c.uuid
+     WHERE c.userId = ?`,
+    [id || ""]
+  ).then((results) => {
+    const fundraises: Record<
+      string,
+      {
+        type: number;
+        agreements: Set<string>;
+        details: Record<string, { label: string; value: string }>;
+      }
+    > = {};
+    (
+      results as {
+        type: number;
+        uuid: string;
+        agreementUuid: string;
+        label: string;
+        value: string;
+        cdUuid: string;
+      }[]
+    ).forEach((r) => {
+      if (fundraises[r.uuid]) {
+        fundraises[r.uuid].type = r.type;
+        fundraises[r.uuid].agreements.add(r.agreementUuid);
+        fundraises[r.uuid].details[r.cdUuid] = {
+          label: r.label,
+          value: r.value,
+        };
+      } else {
+        fundraises[r.uuid] = {
+          type: r.type,
+          agreements: new Set(r.agreementUuid),
+          details: { [r.cdUuid]: { label: r.label, value: r.value } },
+        };
+      }
     });
+    return {
+      fundraises: Object.entries(fundraises).map(([uuid, result]) => ({
+        uuid,
+        type: FUNDRAISE_TYPES[result.type].id,
+        investorCount: result.agreements.size,
+        details: Object.entries(result.details).map(([uuid, det]) => ({
+          ...det,
+          uuid,
+        })),
+        progress: 0,
+      })),
+    };
+  });
 };
 
 export const handler = clerkAuthenticateLambda(
