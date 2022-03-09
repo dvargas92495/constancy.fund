331b8e52e8152702ffa88e08f5a9961ac14a7bf1
David Vargas <dvargas92495@gmail.com>
2022-03-08 18:19:31 -0600

    fix error migration file

--- db/migrations/20220308211143_add_investor_field/migration.sql
+++ db/migrations/20220308211143_add_investor_field/migration.sql
@@ -2,4 +2,4 @@
 ALTER TABLE `agreement` ADD COLUMN `investorUuid` VARCHAR(191) NULL;
 
 UPDATE agreement SET investorUuid = UUID();
-INSERT INTO `Investor` (uuid, name, email) SELECT UUID(), a.name, a.email FROM agreement a;
+INSERT INTO `Investor` (uuid, name, email) SELECT a.investorUuid, a.name, a.email FROM agreement a;
