b45f9f222ead494e4627d7a41553e558cf9fbb08
David Vargas <dvargas92495@gmail.com>
2022-03-14 11:01:50 -0600

    static site 3.3.1

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ provider "github" {
 
 module "aws_static_site" {
   source  = "dvargas92495/static-site/aws"
-  version = "3.3.0"
+  version = "3.3.1"
 
   domain = "constancy.fund"
   secret = var.secret
@@ -105,7 +105,7 @@ module "aws-serverless-backend" {
 
 module "aws_static_site_staging" {
   source  = "dvargas92495/static-site/aws"
-  version = "3.3.0"
+  version = "3.3.1"
 
   domain = "staging.constancy.fund"
   secret = var.secret
