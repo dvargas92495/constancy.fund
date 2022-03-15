4ffe8cae448900121d59557b6f39c78bedae294a
David Vargas <dvargas92495@gmail.com>
2022-03-15 12:12:26 -0600

    static site 3.3.5 plus some logging

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,27 @@
 import { createRequestHandler } from "@dvargas92495/remix-lambda-at-edge";
+import type { CloudFrontRequestEvent } from "aws-lambda";
 // import * as build from "@remix-run/dev/server-build";
 // import sendEmail from "aws-sdk-plus/dist/sendEmail";
 
-export const handler = createRequestHandler({
-  build: require('./build'),
-  originPaths: [
-    "favicon.ico",
-    /^\/build\/.*/,
-    /^\/images\/.*/,
-    /^\/svgs\/.*/,
-    /^\/_contracts\/.*/,
-  ],
-  onError: (e) => console.log("Send email to me", e),
-  /*sendEmail({
+export const handler = (e: CloudFrontRequestEvent, c, cb) => {
+  const request = e.Records[0].cf.request;
+  console.log("URI::", request.uri);
+  console.log("METHOD::", request.method);
+  console.log("HEADERS::", request.headers);
+  return createRequestHandler({
+    build: require("./build"),
+    originPaths: [
+      "favicon.ico",
+      /^\/build\/.*/,
+      /^\/images\/.*/,
+      /^\/svgs\/.*/,
+      /^\/_contracts\/.*/,
+    ],
+    onError: (e) => console.log("Send email to me", e),
+    /*sendEmail({
       to: "support@constancy.fund",
       subject: "Remix Origin Error",
       body: e.message,
     }),*/
-});
+  })(e, c, cb);
+};
