ade490bc8b862f5582ebb72e69edaa50e636c340
David Vargas <dvargas92495@gmail.com>
2022-03-15 18:29:23 -0600

    client uat hack

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,39 @@
 import { createRequestHandler } from "@dvargas92495/remix-lambda-at-edge";
+import { CloudFrontRequestHandler } from "aws-lambda";
 // import * as build from "@remix-run/dev/server-build";
 // import sendEmail from "aws-sdk-plus/dist/sendEmail";
 
-export const handler = createRequestHandler({
-  build: require("./build"),
-  originPaths: [
-    "favicon.ico",
-    /^\/build\/.*/,
-    /^\/images\/.*/,
-    /^\/svgs\/.*/,
-    /^\/_contracts\/.*/,
-  ],
-  onError: (e) => console.log("Send email to me", e),
-  /*sendEmail({
+const originPaths = [
+  "favicon.ico",
+  /^\/build\/.*/,
+  /^\/images\/.*/,
+  /^\/svgs\/.*/,
+  /^\/_contracts\/.*/,
+];
+
+export const handler: CloudFrontRequestHandler = (e, c, cb) => {
+  // TODO: Remove this block when clientUat bug is resolved
+  const cloudfrontRequest = e.Records[0].cf.request;
+  const originPathRegexes = originPaths.map((s) =>
+    typeof s === "string" ? new RegExp(s) : s
+  );
+  if (originPathRegexes.some((r) => r.test(cloudfrontRequest.uri))) {
+    return cb(undefined, cloudfrontRequest);
+  }
+  if (cloudfrontRequest.headers.cookie?.[0]?.value) {
+    cloudfrontRequest.headers.cookie[0].value =
+      cloudfrontRequest.headers.cookie[0].value.replace("__client_uat=0; ", "");
+  }
+  // END TODO
+
+  return createRequestHandler({
+    build: require("./build"),
+    originPaths,
+    onError: (e) => console.log("Send email to me", e),
+    /*sendEmail({
       to: "support@constancy.fund",
       subject: "Remix Origin Error",
       body: e.message,
     }),*/
-});
+  })(e, c, cb);
+};
