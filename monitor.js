d4b58351dc24abbaff711b8e70cf881c44b069ec
David Vargas <dvargas92495@gmail.com>
2022-03-16 17:16:59 -0600

    delay build importing should make all assets load instantly

--- monitor.js
+++ monitor.js
@@ -1,4 +1,20 @@
-const fs = require('fs');
-const outfile = 'out/index.js'
-const content = fs.readFileSync(outfile).toString();
-fs.writeFileSync(outfile, `console.log("First Line of File");\n${content}`);
+const fs = require("fs");
+const contents = fs.readFileSync("out/index.js").toString();
+fs.writeFileSync(
+  "out/index.js",
+  `Object.keys(require.cache).forEach(function(key) {
+  delete require.cache[key];
+}); 
+performance = [{time: process.hrtime.bigint(), message: "started"}];
+${contents}
+performance.push({ time: process.hrtime.bigint(), message: "finished setup" });
+performance.slice(1).forEach((k, i) => {
+  console.log(
+    k.message,
+    "took",
+    Number(k.time - performance[i].time) / 1000000,
+    "ms"
+  );
+});
+`
+);
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,6 @@
 import { createRequestHandler } from "remix-lambda-at-edge";
 import type { CloudFrontRequestHandler } from "aws-lambda";
-// import * as build from "@remix-run/dev/server-build";
-// record 11083271
-// record 10920357
-// import sendEmail from "aws-sdk-plus/dist/sendEmail";
-console.log('Loading handler...');
+
 const originPaths = [
   "favicon.ico",
   /^\/build\/.*/,
@@ -14,7 +10,7 @@ const originPaths = [
 ];
 
 const requestHandler = createRequestHandler({
-  build: require("./build"),
+  getBuild: () => require("./build"),
   originPaths,
   onError: (e) => console.log("Send email to me", e),
   /*sendEmail({
@@ -23,9 +19,7 @@ const requestHandler = createRequestHandler({
     body: e.message,
   }),*/
 });
-console.log('Handler Loaded...');
 
 export const handler: CloudFrontRequestHandler = (e, c, cb) => {
-  console.log('HANDLING', e.Records[0].cf.request.uri, e.Records[0].cf.request.method)
   return requestHandler(e, c, cb);
 }
