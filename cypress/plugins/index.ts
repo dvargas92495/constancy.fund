f3edff7e83575c499735d08f4c750b3c6fba0757
David Vargas <dvargas92495@gmail.com>
2022-04-07 09:57:37 -0400

    change to running a manual cypress command for e2e tests

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -7,6 +7,7 @@ export default (
   config: Cypress.PluginConfigOptions
 ) => {
   config.supportFile = "cypress/support/index.ts";
+  config.projectId = process.env.CYPRESS_PROJECT_ID;
   // config.baseUrl = process.env.HOST;
   config.env.HOST = process.env.HOST;
   config.env.CYPRESS_USER_PASSWORD = process.env.CYPRESS_USER_PASSWORD;
