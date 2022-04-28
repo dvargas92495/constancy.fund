47575d36051e6629446ddd29c082fe79e21ca89e
David Vargas <dvargas92495@gmail.com>
2022-04-28 16:31:51 -0400

    run migration

---
+++ migrations/2022-04-28-04-09-fix-smartcontract-table.ts
@@ -0,0 +1,20 @@
+import type { MigrationProps } from "fuegojs/dist/migrate";
+import getMysqlConnection from "../app/data/mysql.server";
+
+export const migrate = (args: MigrationProps) => {
+  return getMysqlConnection(args.connection).then((cxn) =>
+    cxn
+      .execute(`RENAME TABLE smart_contract TO smart_contracts`)
+      .then(() =>
+        cxn.execute(`ALTER TABLE smart_contracts MODIFY hash VARCHAR(64)`)
+      )
+  );
+};
+
+export const revert = (args: MigrationProps) => {
+  return getMysqlConnection(args.connection).then((cxn) =>
+    cxn
+      .execute(`ALTER TABLE smart_contracts MODIFY hash VARCHAR(36)`)
+      .then(() => cxn.execute(`RENAME TABLE smart_contracts TO smart_contract`))
+  );
+};
